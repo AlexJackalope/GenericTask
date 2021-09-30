44277416325337f3b1244c43abe62a245e4f239a
Alexandra Zaytseva <62497070+AlexJackalope@users.noreply.github.com>
2021-09-30 15:47:54 +0500

    Add files via upload

---
+++ Maze.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace MazeGeneric
+{
+    public class Maze { }
+
+    public class MazeBuilder<TMazeItem> where TMazeItem : MazeBuilder<TMazeItem>
+    {
+        protected Maze maze = new Maze();
+        
+        public TMazeItem AddRock(int x, int y)
+        {
+            // добавляем камень в maze
+            return (TMazeItem)this;
+        }
+    
+        // ещё много подобных методов добавления всего в maze.
+        
+        public Maze Build()
+        {
+            // ...
+            return maze;
+        }
+    }
+    
+    public class ScaryMazeBuilder : MazeBuilder<ScaryMazeBuilder>
+    {
+        // Хотим добавить новые возможности к MazeBuilder-у
+        public ScaryMazeBuilder AddGhost(int x, int y)
+        {
+            return this;
+        }
+
+        // ещё много методов добавления страшилок в лабиринт.
+    }
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestBuilder_ShouldBeOk()
+        {
+            var builder = new ScaryMazeBuilder();
+            var maze = builder
+                .AddRock(5, 6)
+                .AddRock(5, 4)
+                .AddRock(4, 5)
+                .AddGhost(5, 5)
+                .AddGhost(15, 5)
+                .Build();
+        }
+    }
+}
